c49fd8aeb7b167b2e667ec9917594ca89b10285c
Zhila <zhilab1993@gmail.com>
2021-12-24 00:57:52 +0200

    two

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,24 @@
+import {
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway({ namespace: '/chat' })
+export class ChatGateway implements OnGatewayInit {
+  @WebSocketServer() wss: Server;
+
+  private logger: Logger = new Logger('ChatGateway');
+
+  @SubscribeMessage('chatToServer')
+  handleMessage(client: Socket, message: { sender: string; message: string }) {
+    this.wss.emit('chatToClient', message);
+  }
+
+  afterInit(server: any): any {
+    this.logger.log('Initialized!');
+  }
+}
